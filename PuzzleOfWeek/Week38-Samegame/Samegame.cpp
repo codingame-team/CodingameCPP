ab1a7f452e35dae7eab3c05849bd25f4455fa3e6
Philippe Mourey <philippe@mourey.com>
2020-09-25 16:36:19 +0200

    added time counter for each loop to check if limit rules are reached

--- PuzzleOfWeek/Week38-Samegame/Samegame.cpp
+++ PuzzleOfWeek/Week38-Samegame/Samegame.cpp
@@ -31,18 +31,10 @@ int x, y;
 // connected component
 int COUNT;
 
-// Function to change coordinate system
-void trans_coord(int *x, int *y)
-{
-    int bufX, bufY;
-    bufX = *x; bufY = *y;
-    *x = bufY ; *y = n - 1 - bufX;
-}
 // Function checks if a cell is valid i.e it
 // is inside the grid and equal to the key
 bool is_valid(int x, int y, int key, int input[n][m])
 {
-    //trans_coord(&x, &y);
     if (x < n && y < m && x >= 0 && y >= 0 && input[x][y] != -1) {
         if (visited[x][y] == false && input[x][y] == key)
             return true;
@@ -57,8 +49,6 @@ bool is_valid(int x, int y, int key, int input[n][m])
 // connection with key = input[i][j]
 void BFS(int x, int y, int i, int j, int input[n][m])
 {
-    //trans_coord(&i, &j);
-    //trans_coord(&x, &y);
     // terminating case for BFS
     if (x != y)
         return;
@@ -84,7 +74,6 @@ void reset_visited()
 {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
-            //trans_coord(&i, &j);
             visited[i][j] = 0;
         }
 }
@@ -96,7 +85,6 @@ void reset_result(int key, int input[n][m])
 {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            //trans_coord(&i, &j);
             if (visited[i][j] && input[i][j] == key)
                 result[i][j] = visited[i][j];
             else
@@ -105,27 +93,9 @@ void reset_result(int key, int input[n][m])
     }
 }
 
-// function to choose a tile in the result region
-// int *choose_tile()
-// {
-//     int *t = new int[2];
-//     bool loop = true;
-//     for (int i = 0; i < n && loop; i++) {
-//         for (int j = 0; j < m && loop; j++) {
-//             //trans_coord(&i, &j);
-//             if (result[i][j])
-//                 cerr << "i = " << *t << " j = " << *(t+1) << endl;
-//                 *t = i; *(t+1) = j;
-//                 loop = false;
-//         }
-//     }
-//     cerr << "t = " << *t << "; t+1 = " << *(t+1) << endl;
-//     //*t = 3; *(t+1) = 8;
-//     return t;
-// }
 
-// function to print the result 
-void print_result(int res)
+// function to get a tile in one of biggest region
+void get_result(int res)
 {
     cerr << "The largest connected "
     << "component of the grid is :" << res << "\n";
@@ -133,7 +103,6 @@ void print_result(int res)
     // prints the largest component
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            //trans_coord(&i, &j);
             if (result[j][n-1-i]) {
                 cerr << result[j][n-1-i] << " ";
                 x = j; y = n-1-i;
@@ -157,7 +126,6 @@ int computeLargestConnectedGrid(int input[n][m])
     
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            //trans_coord(&i, &j);
             reset_visited();
             COUNT = 0;
             
@@ -185,7 +153,7 @@ int computeLargestConnectedGrid(int input[n][m])
         }
     }
     cerr << "current_max = " << current_max <<endl;
-    print_result(current_max);    
+    get_result(current_max);    
     return(current_max);
 }
 
@@ -195,50 +163,42 @@ int main()
     bool start = true;
     int input[n][m];
     int max_tiles = 0;
-    //int x, y;
     // game loop
     while (1) {
         auto begin = chrono::steady_clock::now();
-        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int color; // Color of the tile
                 cin >> color; cin.ignore();
-                //trans_coord(&i, &j);
                 input[j][n-1-i] = color;
-                //cerr << color;
             }
             cerr << endl;
         }
         max_tiles = computeLargestConnectedGrid(input);
         auto end = chrono::steady_clock::now();
         auto time = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-        // string message = "time: " + std::to_string(time) + " ms";
-        // string alert;
-        // if (!start) {
-        //     if (40 < time && time <= 50)
-        //         alert = "Long near 50ms";
-        //     else if (time <= 40)
-        //         alert = "Good <= 50ms";
-        //     else
-        //         alert = "Bad > 50ms";
-        // }
-        // else {
-        //     start = false;
-        //     if (time/1000 <= 20) 
-        //         alert = "Good <= 20s";
-        //     else
-        //         alert = "Bad > 20s";
-        // }
+        string message = "time: " + std::to_string(time) + " ms";
+        string alert;
+        if (!start) {
+            if (40 < time && time <= 50)
+                alert = "Long near 50ms";
+            else if (time <= 40)
+                alert = "Good <= 50ms";
+            else
+                alert = "Bad > 50ms";
+        }
+        else {
+            start = false;
+            if (time/1000 <= 20) 
+                alert = "Good <= 20s";
+            else
+                alert = "Bad > 20s";
+        }
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
-        // int *t = choose_tile();
-        // x = *t ; y = *(t+1);
-        // trans_coord(&x, &y);
-        //cout << t[0] << " " << t[1] << " " << alert << "\\n" << message << endl; // Selected tile "x y [message]".
-        cout << x << " " << y; // Selected tile "x y [message]".
-        cout << " x=" << x << " y=" << y << "\\n" << "lrl=" << max_tiles << endl; 
-        //delete[] t;
+        cout << x << " " << y << " " << alert << "\\n" << message << endl; // Selected tile "x y [message]".
+        //cout << x << " " << y; // Selected tile "x y [message]".
+        //cout << " x=" << x << " y=" << y << "\\n" << "lrl=" << max_tiles << endl; 
     }
 }
